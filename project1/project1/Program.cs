25a0d743dd027da63281d40d3e7663128b539b48
Bùi Đức Thịnh <buiducthinh9a1@gmail.com>
2023-12-01 13:38:58 +0700

    csharp fix

--- project1/project1/Program.cs
+++ project1/project1/Program.cs
@@ -10,6 +10,11 @@ namespace project1
     {
         static void Main(string[] args)
         {
+            int a = 5;
+            int b=6;
+            int c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();
         }
     }
 }
