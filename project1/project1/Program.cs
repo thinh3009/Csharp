fc55b9ec27dc32d9c81aff96224d858b19ab4b1c
Bùi Đức Thịnh <buiducthinh9a1@gmail.com>
2023-12-03 21:27:22 +0700

    new

--- project1/project1/Program.cs
+++ project1/project1/Program.cs
@@ -14,6 +14,15 @@ namespace project1
             int b=6;
             int c = a + b;
             Console.WriteLine(c);
+            int d;
+            Console.WriteLine("nhap so luong phan tu: ");
+            d=int.Parse(Console.ReadLine());
+            int tong = 0;
+            for (int i=0; i <= d; i++)
+            {
+                tong = tong + i;
+            }
+            Console.WriteLine("tong la: " + tong);
             Console.ReadKey();
         }
     }
